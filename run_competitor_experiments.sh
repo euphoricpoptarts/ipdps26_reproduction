dcb2caa9f849d0261c4ae91670e559c0fe66c902
Mike Gilbert <euphoricpoptarts@gmail.com>
2026-02-19 19:39:01 -0500

    typo fix

--- run_competitor_experiments.sh
+++ run_competitor_experiments.sh
@@ -1,6 +1,7 @@
 g++ -O3 -o metis2gala metis2galabin.cpp
 mkdir comp_data
 python3 python_scripts/run_gala.py graphs/ comp_data/gala_results.txt
-python3 python_scripts/run_vlouvain mtx/ comp_data/vlouv_results.txt
-python3 python_scripts/run_gve_louvain mtx/ comp_data/gve_louvain_results.txt
-python3 python_scripts/run_gve_leiden mtx/ comp_data/gve_leiden_results.txt
+python3 python_scripts/run_vlouvain.py mtx/ comp_data/vlouv_results.txt
+python3 python_scripts/run_gve_louvain.py mtx/ comp_data/gve_louvain_results.txt
+python3 python_scripts/run_gve_leiden.py mtx/ comp_data/gve_leiden_results.txt
+python3 python_scripts/run_vieclus.py graphs/ comp_data/vieclus_results.txt
