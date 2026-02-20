9ef286e684f81d31dfda4b581d9c6cce31840b88
Mike Gilbert <euphoricpoptarts@gmail.com>
2026-02-19 22:31:16 -0500

    typo

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@ snap install cmake --classic
 apt-get install python3-parse
 bash setup_gpu.sh
 bash data_setup.sh
-bash setup_out_code.sh
+bash setup_our_code.sh
 bash setup_competitors.sh
 bash run_our_experiments.sh
 bash run_competitor_experiments.sh
