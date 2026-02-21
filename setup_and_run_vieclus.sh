4e2cb6fefb404e620516fbc59f2d4d101d9e9480
Mike Gilbert <euphoricpoptarts@gmail.com>
2026-02-20 21:53:53 -0500

    specific commit for vieclus

--- setup_and_run_vieclus.sh
+++ setup_and_run_vieclus.sh
@@ -4,6 +4,7 @@ x=${PWD}
 mkdir comp
 git clone https://github.com/VieClus/VieClus.git
 cd ${x}/comp/VieClus
+git checkout 5caa08291b67a9e134ac7b41bc1aacd6605020cd
 bash compile_withcmake.sh
 cp build/evolutionary_clustering ${x}/comp_exe/vieclus
 cd ${x}
